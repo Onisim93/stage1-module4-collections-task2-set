d679bab07cd6972e88f3e684fa7187a3dede655f
onisim_albarov <onisim93@gmail.com>
2022-12-04 15:42:08 +0400

    complete stage1-module4-collections-task2

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -2,8 +2,25 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (int i : sourceList) {
+            if (i % 2 != 0) {
+                result.add(i);
+                result.add(2*i);
+            }
+            else {
+                while (i % 2 == 0) {
+                    result.add(i);
+                    i /= 2;
+                }
+                result.add(i);
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,15 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> result = new HashSet<>(firstSet);
+        result.retainAll(secondSet);
+        Set<String> thirdCopy = new HashSet<>(thirdSet);
+        thirdCopy.removeAll(secondSet);
+        thirdCopy.removeAll(firstSet);
+
+
+        result.addAll(thirdCopy);
+
+        return result;
     }
 }
