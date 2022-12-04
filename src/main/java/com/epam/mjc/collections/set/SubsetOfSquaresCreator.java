0bc50b4fc12fafad0eacfcd9b2197eb55d302752
onisim_albarov <onisim93@gmail.com>
2022-12-04 15:42:43 +0400

    delete unused imports

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,7 +2,6 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class SubsetOfSquaresCreator {
